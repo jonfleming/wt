6bf2f999cbb93d4eebcdfb6c6ea8a62575cf5be6
Jon Fleming <jon@cloud-6ff0cb.managed-vps.net>
2024-09-05 07:38:30 +0000

    path

--- cmd/wt/main.go
+++ cmd/wt/main.go
@@ -4,17 +4,17 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/yourusername/html2text"
+	"github.com/jonfleming/wt"
 )
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("Usage: html2text <URL>")
+		fmt.Println("Usage: wt <URL>")
 		os.Exit(1)
 	}
 
 	url := os.Args[1]
-	text, err := html2text.ConvertURL(url)
+	text, err := wt.ConvertURL(url)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
